1f101e08742c84802a0d916789d5fb0b7c61375f
namni22 <142199912+namni22@users.noreply.github.com>
2023-09-27 18:32:38 +0900

    세팅

---
+++ weple-back/src/main/java/kr/co/weple/WepleBackApplication.java
@@ -0,0 +1,13 @@
+package kr.co.weple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WepleBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WepleBackApplication.class, args);
+	}
+
+}
