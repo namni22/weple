0da932418185a033c5a525e08b328a3ed4484292
YeonJHw <vmflaldjadlaend7@gmail.com>
2023-10-02 06:02:19 +0900

    header
    header 배치

--- weple-front/src/App.js
+++ weple-front/src/App.js
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import "./component/common/common.css";
 import Feed from "./component/feed/Feed";
+import Header from "./component/common/header";
 
 function App() {
   return (
     <div className="weple-wrap">
-      {/* 헤더삽입 */}
+      <Header />
       <div className="weple-content">
         <Routes>
           <Route path="/feed/*" element={<Feed />} />
+          <Route path="/header" element={<Header />} />
         </Routes>
       </div>
       {/* 푸터삽입 */}
