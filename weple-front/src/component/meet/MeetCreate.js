52179ea0fa7e94a0ecfb8eff76a63b9375bbdf05
pondue <142170487+pondue@users.noreply.github.com>
2023-10-03 02:54:17 +0900

    meetSettingFrm css

--- weple-front/src/component/meet/MeetCreate.js
+++ weple-front/src/component/meet/MeetCreate.js
@@ -3,10 +3,16 @@ import MeetSettingFrm from "./MeetSettingFrm";
 
 const MeetCreate = () => {
     const [meetTitle, setMeetTitle] = useState("");
+    const [meetMaterials, setMeetMaterials] = useState("");
     
     return (
         <div>MeetCreate
-            <MeetSettingFrm meetTitle={meetTitle} setMeetTitle={setMeetTitle}/>
+            <MeetSettingFrm 
+                meetTitle={meetTitle} 
+                setMeetTitle={setMeetTitle}
+                meetMaterials={meetMaterials}
+                setMeetMaterials={setMeetMaterials}
+            />
         </div>
 
     );
--- weple-front/src/component/meet/MeetSettingFrm.js
+++ weple-front/src/component/meet/MeetSettingFrm.js
@@ -1,9 +1,13 @@
 import "./meetSettingFrm.css";
 import JwInput from "./meetUtil/JwInputFrm";
+import { JwButton1 } from "../meet/meetUtil/JwButton";
+
 const MeetSettingFrm = (props) => {
     // 모임만들 정보 선언
     const meetTitle = props.meetTitle;
     const setMeetTitle = props.setMeetTitle;
+    const meetMaterials = props.meetMaterials;
+    const setMeetMaterials = props.setMeetMaterials;
 
 
     return (
@@ -28,23 +32,47 @@ const MeetSettingFrm = (props) => {
                     type="file"
                     id="meetThumbnail"
                     accept="image/*"
-                    // 파일선택을 바꿀때 이벤트 발생
-                    //썸네일 미리보기 함수
+                // 파일선택을 바꿀때 이벤트 발생
+                //썸네일 미리보기 함수
                 ></input>
-                
+
             </div>
             <div className="meetDateFrm">
                 <label>모임날짜</label>
                 <input type="datetime-local"></input>
             </div>
-            <div className="meetPlaceFrm">모임위치</div>
+            <div className="meetPlaceFrm">
+                <label>모임위치</label>
+                <div>주소입력 , 지도</div>
+            </div>
             <div className="meetMemberLimitFrm">
                 <label>모임참여인원</label>
                 <input type="number" min="1" max="100"></input>
+
             </div>
-            <div className="meetContentFrm">모임설명에디터</div>
-            <div className="meetMaterialsFrm">준비물</div>
-            <div className="meet-btn-box">버튼박스</div>
+            <div className="meetContentFrm">
+                <label>모임설명</label>
+                <textarea></textarea>
+            </div>
+            <div className="meetMaterialsFrm">
+                <label>준비물</label>
+                <div className="meetMaterials-input-box">
+                    <JwInput
+                        type="text"
+                        data={meetMaterials}
+                        setData={setMeetMaterials}
+                        content="meetTitleFrm"
+                    ></JwInput>
+                </div>
+                <div className="meetMaterialsInsert-btn-box">
+                    <JwButton1 text="추가"></JwButton1>
+                </div>
+                <div className="materialsSetting">준비물1</div>
+
+            </div>
+            <div className="meet-btn-box">
+                <JwButton1 text="모임 생성"></JwButton1>
+            </div>
 
         </div>
     );
