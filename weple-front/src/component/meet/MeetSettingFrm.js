7f4873de344ecb00c1911f82a88d4d8c3b2a2217
pondue <142170487+pondue@users.noreply.github.com>
2023-10-03 01:58:30 +0900

    meet

--- weple-front/src/component/meet/MeetSettingFrm.js
+++ weple-front/src/component/meet/MeetSettingFrm.js
@@ -4,26 +4,44 @@ const MeetSettingFrm = (props) => {
     // 모임만들 정보 선언
     const meetTitle = props.meetTitle;
     const setMeetTitle = props.setMeetTitle;
-    
-    
+
+
     return (
         <div className="meetSettingFrm-wrap">
             <div className="meetCategoriFrm">카테고리</div>
             <div className="meetTitleFrm">
-                <label htmlFor="meetTitleFrm">제목</label>
+                <label htmlFor="meetTitleFrm">모임 이름</label>
                 <div className="jwInput" id="jwInput">
                     <JwInput
                         type="text"
                         data={meetTitle}
                         setData={setMeetTitle}
-                        content="meetTitleFrm" 
+                        content="meetTitleFrm"
                     />
                 </div>
             </div>
-            <div className="meetThumbnailFrm">썸네일</div>
-            <div className="meetDateFrm">모임날짜</div>
+            <div className="meetThumbnailFrm">
+                <label>썸네일</label>
+                {/* accept = image/* : 이미지파일만 올릴수 있도록 */}
+                <input
+                    className="meetThumbnail-input"
+                    type="file"
+                    id="meetThumbnail"
+                    accept="image/*"
+                    // 파일선택을 바꿀때 이벤트 발생
+                    //썸네일 미리보기 함수
+                ></input>
+                
+            </div>
+            <div className="meetDateFrm">
+                <label>모임날짜</label>
+                <input type="datetime-local"></input>
+            </div>
             <div className="meetPlaceFrm">모임위치</div>
-            <div className="meetMemberLimitFrm">모임참여인원</div>
+            <div className="meetMemberLimitFrm">
+                <label>모임참여인원</label>
+                <input type="number" min="1" max="100"></input>
+            </div>
             <div className="meetContentFrm">모임설명에디터</div>
             <div className="meetMaterialsFrm">준비물</div>
             <div className="meet-btn-box">버튼박스</div>
