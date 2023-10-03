02837684f6687b0bd9f29eb360c28c9f3af5645f
YeonJHw <vmflaldjadlaend7@gmail.com>
2023-10-03 19:08:40 +0900

    Header 파일명 변경

--- weple-front/src/App.js
+++ weple-front/src/App.js
@@ -5,6 +5,7 @@ import Main from "./component/common/Main";
 import Footer from "./component/common/Footer";
 import MeetMain from "./component/meet/MeetMain";
 import Board from "./component/board/Board";
+import Header from "./component/common/Header";
 
 
 
