46a0ea0fdefe3a1549ca92ab91254f8eb45cdae4
pondue <142170487+pondue@users.noreply.github.com>
2023-10-04 01:57:43 +0900

    meetList

--- weple-front/src/component/meet/MeetMain.js
+++ weple-front/src/component/meet/MeetMain.js
@@ -3,6 +3,7 @@ import "./meetDefault.css"
 import { JwButton1 } from "./meetUtil/JwButton";
 import MeetSettingFrm from "./MeetSettingFrm";
 import MeetCreate from "./MeetCreate";
+import MeetList from "./MeetList";
 
 const MeetMain = () => {
     return (
@@ -11,10 +12,12 @@ const MeetMain = () => {
             <Link to="/meet">모임메인link</Link>
             <Link to="/meet/meetCreate">임시모임생성Frmlink</Link>
             <Link to="/meet/meetSettingFrm">임시모임생성link</Link>
+            <Link to="/meet/meetList">모임리스트</Link>
 
             <Routes>
                 <Route path="/meetSettingFrm" element={<MeetSettingFrm />} />
-                <Route path="/meetCreate" element={<MeetCreate/>}></Route>
+                <Route path="/meetCreate" element={<MeetCreate />}></Route>
+                <Route path="/meetList" element={<MeetList />}></Route>
             </Routes>
         </div>
 
