e64af94ba696ae9c280769920b26af5d18908cec
wngpodong <142201273+wngpodong@users.noreply.github.com>
2023-10-04 01:28:22 +0900

    admin 수정

---
+++ weple-front/src/component/admin/AdminBoard.js
@@ -0,0 +1,19 @@
+
+import "./admin.css";
+const AdminBoard = () => {
+  
+  
+    return(
+    <div className="admin-board-wrap">
+        <div className="admin-board-top">   
+        <div className="admin-menu-title"><h1>공지목록</h1></div>   
+        </div>
+    </div>
+  
+
+  
+    )
+    
+};
+
+export default AdminBoard;
