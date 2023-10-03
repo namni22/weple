f9ddc533cbbe3aae4b253c9c489524a0f115a484
kyungsik1101 <142005877+kyungsik1101@users.noreply.github.com>
2023-10-04 02:17:49 +0900

    캘린더 조정

--- weple-front/src/component/meet/MeetCalendar.js
+++ weple-front/src/component/meet/MeetCalendar.js
@@ -2,12 +2,15 @@ import "./afterMeet.css"
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import { JwButton1 } from "./meetUtil/JwButton";
 const MeetCalendar =  ()=>{
     return (
-        <div>
-            <div>캘린더</div>
-            <div>
+        <div className="meetCalendar-all-wrap">
+            <div className="meetCalendar">
                 <Calendar/>
+            </div>
+            <div className="meetCalendar-btn">
+                <JwButton1 text={"일정등록하기"}/>
             </div>
         </div>
     )
