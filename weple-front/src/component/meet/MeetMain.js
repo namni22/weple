f0d912c1733a9598fbd83dbc5cff5df73276b543
kyungsik1101 <142005877+kyungsik1101@users.noreply.github.com>
2023-10-04 14:57:53 +0900

    커밋

--- weple-front/src/component/meet/MeetMain.js
+++ weple-front/src/component/meet/MeetMain.js
@@ -7,10 +7,11 @@ import MeetList from "./MeetList";
 
 import AfterMeet from "./AfterMeet";
 import { useState } from "react";
+import MyMeetList from "./MyMeetList";
 
 const MeetMain = () => {
   const [meetNavi, setMeetNavi] = useState([
-    { url: "afterMeet", text: "내가 개설한 모임", active: false },
+    { url: "myMeet", text: "내가 개설한 모임", active: false },
     { url: "currentMeet", text: "참여하는 모임", active: false },
     { url: "endMeet", text: "종료된 모임", active: false },
   ]);
@@ -38,6 +39,7 @@ const MeetMain = () => {
         <Route path="meetCreate" element={<MeetCreate />}></Route>
         {/* */}
         <Route path="afterMeet/*" element={<AfterMeet />} />
+        <Route path="myMeet" element={<MyMeetList />} />
       </Routes>
     </div>
   );
---
+++ weple-front/src/component/meet/MyMeetList.js
@@ -0,0 +1,11 @@
+import "./afterMeet.css";
+
+const MyMeetList = () => {
+  return (
+    <div>
+      <div>내가 개설한 모임 리스트 출력하는 페이지</div>
+    </div>
+  );
+};
+
+export default MyMeetList;
