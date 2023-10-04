2bf1c9277d64c6f06c7e8f5a27c0dc55f9324679
chaeyeon <142184204+Suibbsu@users.noreply.github.com>
2023-10-04 18:03:58 +0900

    이용약관

--- weple-front/src/App.js
+++ weple-front/src/App.js
@@ -6,9 +6,9 @@ import Footer from "./component/common/Footer";
 import MeetMain from "./component/meet/MeetMain";
 import Board from "./component/board/Board";
 import Admin from "./component/admin/Admin";
-import Join from "./component/member/Join";
 import CategoriMain from "./component/categori/CategoriMain";
 import Header from "./component/common/Header";
+import Join from "./component/member/Join";
 
 function App() {
   return (
