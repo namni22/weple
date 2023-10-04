643d372b74abb0f1cebec1afd3a44d36131968ba
pondue <142170487+pondue@users.noreply.github.com>
2023-10-04 17:31:44 +0900

    모임개설 텍스트 에디터

--- weple-front/src/component/meet/MeetSettingFrm.js
+++ weple-front/src/component/meet/MeetSettingFrm.js
@@ -2,6 +2,7 @@ import "./meetSettingFrm.css";
 import JwInput from "./meetUtil/JwInputFrm";
 import { JwButton1 } from "../meet/meetUtil/JwButton";
 import { Button1, Button2 } from "../util/Button";
+import TextEditor from "../util/TextEditor";
 
 const MeetSettingFrm = (props) => {
     // 모임만들 정보 선언
@@ -9,84 +10,89 @@ const MeetSettingFrm = (props) => {
     const setMeetTitle = props.setMeetTitle;
     const meetMaterials = props.meetMaterials;
     const setMeetMaterials = props.setMeetMaterials;
-
+    const buttonEvent = props.buttonEvent;
 
     return (
         <div className="meetSettingFrm-main-wrap">
 
-        <div className="meetSettingFrm-wrap">
-            <div className="meetCategoriFrm">카테고리</div>
-            <div className="meetTitleFrm">
-                <label htmlFor="meetTitleFrm">모임 이름</label>
-                <div className="jwInput" id="jwInput">
-                    <JwInput
-                        type="text"
-                        data={meetTitle}
-                        setData={setMeetTitle}
-                        content="meetTitleFrm"
-                    />
-                </div>
-            </div>
-            <div className="meetThumbnailFrm">
-                <label>썸네일</label>
-                <div>
-                    {/* accept = image/* : 이미지파일만 올릴수 있도록 */}
-                    <input
-                        className="meetThumbnail-input"
-                        type="file"
-                        id="meetThumbnail"
-                        accept="image/*"
-                    // 파일선택을 바꿀때 이벤트 발생
-                    //썸네일 미리보기 함수
-                    ></input>
-                </div>
-
-            </div>
-            <div className="meetDateFrm">
-                <label>모임날짜</label>
-                <input type="datetime-local"></input>
-            </div>
-            <div className="meetPlaceFrm">
-                <label>모임위치</label>
-                <div>주소입력 , 지도</div>
-            </div>
-            <div className="meetMemberLimitFrm">
-                <label>모임참여인원</label>
-                <input type="number" min="1" max="100"></input>
-
-            </div>
-            <div className="meetContentFrm">
-                <label>모임설명</label>
-                <textarea></textarea>
-            </div>
-            <div className="meetMaterialsFrm">
-                <label>준비물</label>
-                <div className="meetMaterials-content-wrap">
-                    <div className="meetMaterials-input-box">
+            <div className="meetSettingFrm-wrap">
+                <div className="meetCategoriFrm">카테고리</div>
+                <div className="meetTitleFrm">
+                    <label htmlFor="meetTitleFrm">모임 이름</label>
+                    <div className="jwInput" id="jwInput">
                         <JwInput
                             type="text"
-                            data={meetMaterials}
-                            setData={setMeetMaterials}
+                            data={meetTitle}
+                            setData={setMeetTitle}
                             content="meetTitleFrm"
-                        ></JwInput>
+                        />
                     </div>
-                    <div className="meetMaterialsInsert-btn-box">
-                        <Button2 text="추가"></Button2>
+                </div>
+                <div className="meetThumbnailFrm">
+                    <label>썸네일</label>
+                    <div>
+                        {/* accept = image/* : 이미지파일만 올릴수 있도록 */}
+                        <input
+                            className="meetThumbnail-input"
+                            type="file"
+                            id="meetThumbnail"
+                            accept="image/*"
+                        // 파일선택을 바꿀때 이벤트 발생
+                        //썸네일 미리보기 함수
+                        ></input>
                     </div>
 
-                    <div className="meetMaterials-wrap">
-                        <div className="meetMaterials-one">준비물1</div>
-                        <div className="meetMaterials-one">준비물2</div>
-                        <div className="meetMaterials-one">준비물3</div>
+                </div>
+                <div className="meetDateFrm">
+                    <label>모임날짜</label>
+                    <input type="datetime-local"></input>
+                </div>
+                <div className="meetPlaceFrm">
+                    <label>모임위치</label>
+                    <div>주소입력 , 지도</div>
+                </div>
+                <div className="meetMemberLimitFrm">
+                    <label>모임참여인원</label>
+                    <input type="number" min="1" max="100"></input>
+
+                </div>
+                <div className="meetContentFrm">
+                    <label>모임설명</label>
+                    <TextEditor
+
+                    />
+
+
+
+                </div>
+                <div className="meetMaterialsFrm">
+                    <label>준비물</label>
+                    <div className="meetMaterials-content-wrap">
+                        <div className="meetMaterials-input-box">
+                            <JwInput
+                                type="text"
+                                data={meetMaterials}
+                                setData={setMeetMaterials}
+                                content="meetTitleFrm"
+                            ></JwInput>
+                        </div>
+                        <div className="meetMaterialsInsert-btn-box">
+                            <Button2 text="추가"></Button2>
+                        </div>
+
+                        <div className="meetMaterials-wrap">
+                            <div className="meetMaterials-one">준비물1</div>
+                            <div className="meetMaterials-one">준비물2</div>
+                            <div className="meetMaterials-one">준비물3</div>
+                        </div>
                     </div>
+
+                </div>
+                <div className="meet-btn-box">
+                    <Button1 text="모임생성" clickEvent={buttonEvent}></Button1>
                 </div>
 
             </div>
-            <div className="meet-btn-box">
-                <Button1 text="모임생성"></Button1>
-            </div>
-
-        </div>
         </div>
     );
 
