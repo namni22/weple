e3b83bc15d7ad04fa9957608e637571cf54a3bf2
chaeyeon <142184204+Suibbsu@users.noreply.github.com>
2023-10-04 12:04:56 +0900

    커밋

--- weple-front/src/component/member/Join.js
+++ weple-front/src/component/member/Join.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const Join = () => {
+  const [allAgree, setAllAgree] = useState(false);
+  const [agreement, setAgreement] = useState({
+    termsAgree: false,
+    personalInfoAgree: false,
+    eventAlarmAgree: false,
+  });
+
+  const agreeChecked = (event) => {
+    const { name, checked } = event.target;
+    setAgreement((prevAgreement) => ({ ...prevAgreement, [name]: checked }));
+  };
   return (
     <div className="join-wrap">
-      <h3>이용약관</h3>
+      <div className="join-title">
+        <h3>이용약관</h3>
+      </div>
       <div className="join-content"></div>
     </div>
   );
