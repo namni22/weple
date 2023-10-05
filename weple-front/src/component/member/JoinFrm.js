092c6e598646a04fe9a414efacd265624c3dd8e5
chaeyeon <142184204+Suibbsu@users.noreply.github.com>
2023-10-05 10:41:16 +0900

    member back 설정

--- weple-front/src/component/member/JoinFrm.js
+++ weple-front/src/component/member/JoinFrm.js
@@ -1,7 +1,41 @@
+import Input from "../util/InputFrm";
+
 const JoinFrm = () => {
   return (
     <div className="joinFrm">
-      <div className="joinFrm-content">회원가입</div>
+      <div className="joinFrm-content">
+        <JoinInputWrap />
+      </div>
+    </div>
+  );
+};
+
+const JoinInputWrap = (props) => {
+  const data = props.data;
+  const setData = props.setData;
+  const type = props.type;
+  const content = props.content;
+  const label = props.label;
+  const blurEvent = props.blurEvent;
+  const checkMsg = props.checkMsg;
+
+  return (
+    <div className="join-input-wrap">
+      <div>
+        <div className="label">
+          <label htmlFor={content}>{label}</label>
+        </div>
+        <div className="input">
+          <Input
+            type={type}
+            data={data}
+            setData={setData}
+            content={content}
+            blurEvent={blurEvent}
+          />
+        </div>
+      </div>
+      <div className="check-msg">{checkMsg}</div>
     </div>
   );
 };
