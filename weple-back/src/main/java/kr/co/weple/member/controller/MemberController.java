7bf17f7178019a1b4aa65924a39cf4dc9f1e68b3
chaeyeon <142184204+Suibbsu@users.noreply.github.com>
2023-10-05 16:14:51 +0900

    application.properties port 수정

--- weple-back/src/main/java/kr/co/weple/member/controller/MemberController.java
+++ weple-back/src/main/java/kr/co/weple/member/controller/MemberController.java
@@ -2,14 +2,28 @@ package kr.co.weple.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.weple.member.model.service.MemberService;
+import kr.co.weple.member.model.vo.Member;
 
 @Controller
 @RequestMapping(value="/member")
 public class MemberController {
 	@Autowired
 	private MemberService memberService;
+	
+	@GetMapping(value="/checkId/{memberId}")
+	public int checkId(@PathVariable String memberId) {
+		Member m = memberService.selectOneMember(memberId);
+		if(m == null) {
+			return 0;
+		}else {
+			return 1;
+		}
+		
+	}
 
 }
