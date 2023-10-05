37026047a97ae0b1fb61f53652e96cbe0ee3fee7
yeonju <vmflaldjadlaend7@gmail.com>
2023-10-05 17:05:27 +0900

    Review 리스트 생성

--- weple-front/src/component/review/ReviewWriteFrm.js
+++ weple-front/src/component/review/ReviewWriteFrm.js
@@ -1,30 +1,30 @@
 import "./review.css";
-const ReviewWriteFrm = ()=>{
-    return (
-      <div className="review-write-wrap">
-        <div className="review-top">
-          <div className="profileImg">
-            <img src="./img/profile_default.png"></img>
-          </div>
-          <div className="review-star">
-            <span class="material-icons">star</span>
-            <span class="material-icons">star_half</span>
-            <span class="material-icons">star_border</span>
-            <span class="material-icons">star_border</span>
-            <span class="material-icons">star_border</span>
-          </div>
+const ReviewWriteFrm = () => {
+  return (
+    <div className="review-write-wrap">
+      <div className="review-top">
+        <div className="profileImg">
+          <img src="./img/profile_default.png"></img>
         </div>
-        <textarea
-          className="review-write"
-          placeholder="내용을 입력하세요."
-        ></textarea>
-        <div className="review-bottom">
-          <img className="reviewImg"></img>
-          <img className="reviewImg"></img>
-          <span class="material-icons plus">add</span>
+        <div className="star">
+          <span class="material-icons">star</span>
+          <span class="material-icons">star_half</span>
+          <span class="material-icons">star_border</span>
+          <span class="material-icons">star_border</span>
+          <span class="material-icons">star_border</span>
         </div>
-        <button>등록하기</button>
       </div>
-    );
-}
+      <textarea
+        className="review-write"
+        placeholder="내용을 입력하세요."
+      ></textarea>
+      <div className="review-bottom">
+        <img className="reviewImg"></img>
+        <img className="reviewImg"></img>
+        <span class="material-icons plus">add</span>
+      </div>
+      <button>등록하기</button>
+    </div>
+  );
+};
 export default ReviewWriteFrm;
