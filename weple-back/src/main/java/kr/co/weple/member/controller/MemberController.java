02142d4b20f759af3dfd2f5610d5d871cfbaf878
chaeyeon <142184204+Suibbsu@users.noreply.github.com>
2023-10-05 16:17:57 +0900

    member back 수정

--- weple-back/src/main/java/kr/co/weple/member/controller/MemberController.java
+++ weple-back/src/main/java/kr/co/weple/member/controller/MemberController.java
@@ -1,15 +1,15 @@
 package kr.co.weple.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.weple.member.model.service.MemberService;
 import kr.co.weple.member.model.vo.Member;
 
-@Controller
+@RestController
 @RequestMapping(value="/member")
 public class MemberController {
 	@Autowired
