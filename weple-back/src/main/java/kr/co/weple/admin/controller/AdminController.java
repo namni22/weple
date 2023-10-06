b5a85328a51f3458033f2683137f46813426824a
wngpodong <wngpodong@naver.com>
2023-10-06 17:32:37 +0900

    admin컨트롤러 만듦

--- weple-back/src/main/java/kr/co/weple/admin/controller/AdminController.java
+++ weple-back/src/main/java/kr/co/weple/admin/controller/AdminController.java
@@ -1,4 +1,4 @@
-package kr.co.weple.member.controller;
+package kr.co.weple.admin.controller;
 
 import java.util.List;
 import java.util.Map;
@@ -11,28 +11,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import kr.co.weple.member.model.service.MemberService;
+import kr.co.weple.admin.model.service.AdminService;
 import kr.co.weple.member.model.vo.Member;
 
 @RestController
-@RequestMapping(value="/member")
-public class MemberController {
+@RequestMapping(value="/admin")
+public class AdminController {
 	@Autowired
-	private MemberService memberService;
+	private AdminService adminService;
 	
-	@GetMapping(value="/checkId/{memberId}")
-	public int checkId(@PathVariable String memberId) {
-		Member m = memberService.selectOneMember(memberId);
-		if(m == null) {
-			return 0;
-		}else {
-			return 1;
-		}
-		
-	}
-	@GetMapping(value="/categoryList")
-	public List categoryList() {
-		return memberService.categoryList();
+	//멤버리스트조회
+	@GetMapping(value="/memberList/{reqPage}")
+	public Map list(@PathVariable int reqPage) {
+		return adminService.memberList(reqPage);
 	}
 	
+	
+	
+
+	//멤버 등급 변경
+
+	@PostMapping(value="/changeMemberGrade")
+	public int changeMemberGrade(@RequestBody Member member) {
+		return adminService.changeMemberGrade(member);
+	}
+
 }
