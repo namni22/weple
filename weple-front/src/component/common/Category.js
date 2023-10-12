b3d9e367b321a564cfab54322da2f7c7f48fc7bb
pondue <142170487+pondue@users.noreply.github.com>
2023-10-12 15:38:01 +0900

    카테고리 대분류 출력

--- weple-front/src/component/common/Category.js
+++ weple-front/src/component/common/Category.js
@@ -7,39 +7,39 @@ const Category = () => {
   return (
     <div className="category-wrap">
       <div className="a">카테고리</div>
-      <Link to="/meet/meetList" state={{ bigCategoryNo: 1 }}>
+      <Link to="/meet/meetList" state={{ bigCategoryNo: 1, bigCategoryName: "스포츠" }}>
         <div className="category">
           <img src="../img/category/sport.jpg"></img>
           <span>스포츠</span>
         </div>
       </Link>
-      <Link to="/meet/meetList" state={{ bigCategoryNo: 30 }}>
+      <Link to="/meet/meetList" state={{ bigCategoryNo: 30, bigCategoryName: "여행" }}>
         <div className="category">
           <img src="../img/category/travel.jpg"></img>
           <span>여행</span>
         </div>
       </Link>
 
-      <Link to="/meet/meetList" state={{ bigCategoryNo: 14 }}>
+      <Link to="/meet/meetList" state={{ bigCategoryNo: 14, bigCategoryName: "요리" }}>
         <div className="category">
           <img src="../img/category/cook.jpg"></img>
           <span>요리</span>
         </div>
       </Link>
-      <Link to="/meet/meetList" state={{ bigCategoryNo: 8 }}>
+      <Link to="/meet/meetList" state={{ bigCategoryNo: 8, bigCategoryName: "공예DIY" }}>
         <div className="category">
           <img src="../img/category/DIY.jpg"></img>
           <span>공예DIY</span>
         </div>
       </Link>
-      <Link to="/meet/meetList" state={{ bigCategoryNo: 25 }}>
+      <Link to="/meet/meetList" state={{ bigCategoryNo: 25, bigCategoryName: "자기개발" }}>
         <div className="category">
           <img src="../img/category/selfdevelop.jpg"></img>
           <span>자기계발</span>
         </div>
       </Link>
 
-      <Link to="/meet/meetList" state={{ bigCategoryNo: 19 }}>
+      <Link to="/meet/meetList" state={{ bigCategoryNo: 19, bigCategoryName: "문화예술" }}>
         <div className="category">
           <img src="../img/category/movie.jpg"></img>
           <span>문화예술</span>
