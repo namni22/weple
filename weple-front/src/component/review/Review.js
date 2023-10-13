65be70625afe5c2f8548f1b75bb53c09bf1552a2
yeonju <vmflaldjadlaend7@gmail.com>
2023-10-13 16:51:06 +0900

    REVIEWLIST.CSS

--- weple-front/src/component/review/Review.js
+++ weple-front/src/component/review/Review.js
@@ -1,13 +1,25 @@
 import { useEffect, useState } from "react";
 import SwiperComponent from "../util/Swiper";
 import "./review.css";
+import "./reviewList.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
-
+const starRating = () => {
+  const result = [];
+  for (let i = 0; i < 5; i++) {
+    result.push(
+      <span className="material-icons" key={"starRating" + i}>
+        grade
+      </span>
+    );
+  }
+  return result;
+};
 const Review = (props) => {
   // const reviewList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //10개의 object
   const [reviewList, setReviewList] = useState([]);
   const meetNo = props.meetNo;
+  const meetStar = 3.2;
   useEffect(() => {
     axios
       .get("/review/reviewList/" + meetNo)
@@ -33,14 +45,26 @@ const Review = (props) => {
   return (
     <div className="review-wrap">
       <div className="review-top">
-        <div className="star-wrap">
+        <div className="meet-star-wrap">
+          <div className="star-rating meet-star">
+            <div
+              className="star-rating-fill"
+              style={{ width: (meetStar / 5) * 100 + "%" }}
+            >
+              {starRating()}
+            </div>
+            <div className="star-rating-base">{starRating()}</div>
+          </div>
+          <div className="meet-star-score">1.4</div>
+        </div>
+        {/* <div className="star-wrap">
           <span className="material-icons star">star</span>
           <span className="material-icons star">star</span>
           <span className="material-icons star">star</span>
           <span className="material-icons star">star</span>
           <span className="material-icons star">star</span>
           <span className="star-rating">5.0</span>
-        </div>
+        </div> */}
         <Link to="/reviewList">
           <div className="more-btn">props.개 후기 더보기</div>
         </Link>
