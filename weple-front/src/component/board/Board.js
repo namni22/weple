dbf1a4da0d75de3c127ba5c6c4034516539f63c9
wngpodong <142201273+wngpodong@users.noreply.github.com>
2023-10-16 03:46:10 +0900

    BoardTab 추가 및 작업

--- weple-front/src/component/board/Board.js
+++ weple-front/src/component/board/Board.js
@@ -1,19 +1,34 @@
 import { Route, Routes } from "react-router-dom";
 import "./board.css";
-import BoardList from "./BoardList";
-import BoardView from "./BoardView";
+import { BoardAll, BoardEvent, BoardFaq, BoardNotice } from "./BoardList";
+import { useState } from "react";
+import BoardTab from "./BoardTab";
+
 
 
 const Board = () => {
+  const [tabs, setTabs] = useState([
+    { url: "boardAll", text: "전체", active: false },
+    { url: "boardNotice", text: "공지", active: false },
+    { url: "boardEvent", text: "이벤트", active: false },
+    { url: "boardFaq", text: "FAQ", active: false },
+  ]);
+
+  tabs[0].active = true;
 
   return (
     <div className="board-all-wrap">
       <div className="board-title"><h1>공지사항</h1></div>
+      <BoardTab tabs={tabs} setTabs={setTabs}/>
       <Routes>
-        <Route path="view" element={<BoardView />} />
-        <Route path="*" element={<BoardList />} />
+        <Route path="boardAll" element={<BoardAll boardType = {99} tabs = {tabs}/>} />
+        <Route path="boardNotice" element={<BoardEvent boardType = {0} tabs = {tabs}/>} />
+        <Route path="boardEvent" element={<BoardFaq boardType = {1} tabs = {tabs}/>} />
+        {/* <Route path="boardFaq" element={<BoardNotice boardType = {2} tabs = {tabs}/>} /> */}
+        <Route path="*" element={<BoardAll boardType = {99} tabs = {tabs}/>}/>
       </Routes>
     </div>
   );
 };
+
 export default Board;
