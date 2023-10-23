6e4ce0ef2ff5585413f7049e90e5e928f0c2d276
yeonju <vmflaldjadlaend7@gmail.com>
2023-10-23 10:59:21 +0900

    사이드바 수정

--- weple-front/src/App.js
+++ weple-front/src/App.js
@@ -81,6 +81,7 @@ function App() {
                 isLogin={isLogin}
                 setIsAdmin={setIsAdmin}
                 setIsLogin={setIsLogin}
+                isAdmin={isAdmin}
               />
             }
           />
